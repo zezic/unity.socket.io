79a0505d8aa5fda86f2082c3b2495403e10fb752
Nishant <nishantkumar2004@gmail.com>
2016-07-23 20:39:36 -0700

    Adding a VS2015 Solution to build the SocketIO plugin dll to prevent LGPL conflict

---
+++ SocketIO/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("SocketIOPlugin")]
+[assembly: AssemblyDescription("Unity Plugin for SocketIO")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SocketIOPlugin.dll")]
+[assembly: AssemblyCopyright("Copyright Nishant Naveen 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("1.0.2.*")]
+
+// The following attributes are used to specify the signing key for the assembly, 
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
